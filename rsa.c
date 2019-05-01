ac634e4621482a3053741d3942d13c37e1a34767
virajdoshi <44646580+virajdoshi@users.noreply.github.com>
2019-05-01 22:12:14 +0530

    Update rsa.c

--- rsa.c
+++ rsa.c
@@ -1,15 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
 void main()
 {
-	int p=53,q=59;					//Select 2 prime numbers 
+	int p=61,q=53;					//Select 2 prime numbers 
 	int n=p*q;						
-	float phi=(p-1)*(q-1);       
-	printf("The value of phi = %f\n", phi);
+	int phi=(p-1)*(q-1);       
+	printf("The value of phi = %d\n", phi);
 	
-	int e=3;						
+	int e;
+	for(int i = 2; i<phi; i++)
+	{
+	    if(phi % i != 0)
+	    {
+	        e=i;
+	        break;
+	    }
+	}
 	printf("The Value of E is %d\n",e);
 	
-	int k = 2;                        //for some integers calculation k < e
+	int k = rand()%e;                        //for some integers calculation k < e
 	int d = (1 + (k*phi))/e;
 	printf("The Value of D is %d\n",d);
 		
